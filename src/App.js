68d76e785da1c6c12679e3f2661314260393a8ec
chawit rittiya <chawitmobile@gmail.com>
2022-01-31 22:29:03 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const [file, setFile] = useState()
+  const cameraRef = useRef(null)
+
+  const onChange = (e) => {
+    const file = e.target.files[0];
+    setFile(file)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
+      <input
+        accept='image/*'
+        id='icon-button-file'
+        type='file'
+        capture='environment'
+        onChange={(e) => onChange(e)}
+        ref={cameraRef}
+      />
+      {
+        file &&
+        <img src={URL.createObjectURL(file)} alt='imgFromCamera' width='360px' />
+      }
+      <button
+        onClick={() => {
+          cameraRef.current.value = "";
+          setFile()
+        }}
+      >
+        delete media
+      </button>
     </div>
   );
 }
